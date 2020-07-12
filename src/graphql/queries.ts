721ee3911ed6d40898eefe96ee29a13838b9a889
Moise <moisecla@amazon.com>
2020-07-12 16:52:04 -0500

    updated all packages which were causing build failures

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,34 +1,37 @@
-// tslint:disable
+/* tslint:disable */
+/* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const listPokemon = `query ListPokemon($limit: Int, $nextToken: Int) {
-  listPokemon(limit: $limit, nextToken: $nextToken) {
-    nextToken
-    items {
+export const listPokemon = /* GraphQL */ `
+  query ListPokemon($limit: Int, $nextToken: Int) {
+    listPokemon(limit: $limit, nextToken: $nextToken) {
+      nextToken
+      items {
+        baseExp
+        height
+        id
+        image
+        name
+        weight
+      }
+    }
+  }
+`;
+export const pokemon = /* GraphQL */ `
+  query Pokemon($id: Int) {
+    pokemon(id: $id) {
+      abilities {
+        id
+      }
       baseExp
       height
       id
       image
+      moves {
+        id
+      }
       name
       weight
     }
   }
-}
-`;
-export const pokemon = `query Pokemon($id: Int) {
-  pokemon(id: $id) {
-    abilities {
-      id
-    }
-    baseExp
-    height
-    id
-    image
-    moves {
-      id
-    }
-    name
-    weight
-  }
-}
 `;
